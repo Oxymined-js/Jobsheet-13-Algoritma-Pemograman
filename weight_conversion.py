1a040330ca9d4b9217be2f6cfb79773721b7cfa7
Oxymine-js <lordsaber08@gmail.com>
2024-11-23 21:57:12 +0700

    commit pertama

---
+++ weight_conversion.py
@@ -0,0 +1,9 @@
+def weight_conversion
+berat = int(input("Masukkan berat anda > "))
+satuan = int(input("Dalam satuan apa berat yang anda masukkan ? (K untuk K6, L untuj=k LSB)> "))
+
+if satuan.lower() == 'L':
+    print(f"Berat anda dikonversi menjadi kilogram adalah {round(berat * 0.453592)} kg")
+elif satuan.lower() == 'k':
+    print(f"Berat anda dikonversi menjadi pons adalah {round(berat * 2.20462)} kg")    
+
